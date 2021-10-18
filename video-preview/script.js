5e8fc9bd2edfdc2e6db0b953c3f3f33ccfd1127d
HaleyAllen <haleyallen2074@gmail.com>
2021-10-18 11:27:20 -0700

    initial commit

---
+++ video-preview/script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+
+var vid = document.getElementById("video");
+
+function playMute(element){
+    element.play()
+}
+
+function stopPlay(element){
+    element.pause()
+}
