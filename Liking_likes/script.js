cd28b4c8f896a7141753d21c3b3349482c8f379f
HaleyAllen <haleyallen2074@gmail.com>
2021-10-18 20:27:53 -0700

    inital commit

---
+++ Liking_likes/script.js
@@ -0,0 +1,21 @@
+
+var likes = 0
+
+function clicks(element) {
+    likes += 1;
+    document.getElementById("likes").innerHTML = likes;
+};
+
+var likesTwo = 0
+
+function clicksTwo(element) {
+    likesTwo += 1;
+    document.getElementById("likesTwo").innerHTML = likesTwo;
+};
+
+var likesThree = 0
+
+function clicksThree(element) {
+    likesThree += 1;
+    document.getElementById("likesThree").innerHTML = likesThree;
+};
