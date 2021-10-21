642bd9151dfbc9b7fc121076bbaee916ccf84183
HaleyAllen <haleyallen2074@gmail.com>
2021-10-20 19:55:28 -0700

    inital commit

--- Dojo_Weather/script.js
+++ Dojo_Weather/script.js
@@ -1,1 +1,36 @@
 console.log("page loaded")
+
+function guessWhat(){
+    alert("Loading weather reoprt")
+}
+
+function closeOne(element) {
+    var div = document.getElementById("acceptAllCookies").innerHTML = acceptAllCookies;
+    div.style.display = 'none'
+}
+
+function dropdown(element){
+    if (element.value === "farenhiet") {
+        var elementList = document.querySelector("#hi")
+        elementList.innerText = "78°"
+
+    }
+}
+
+function dropdown(element){
+    if (element.value === "farenhiet") {
+        var elementList = document.querySelector(".lo")
+        for(var i = 0; i < elementList.length; i++){
+            elementList[i].innerText = "80"
+        }
+    }
+}
+
+function dropdown(element){
+    if (element.value === "farenhiet") {
+        var elementList = document.querySelector(".hi")
+        for(var i = 0; i < elementList.length; i++){
+            elementList[i].innerText = "80"
+        }
+    }
+}
