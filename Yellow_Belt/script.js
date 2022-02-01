00bd21d49558b7a8997a2cafe6b084204d25f55f
HaleyAllen <haleyallen2074@gmail.com>
2022-01-31 19:55:14 -0800

    commit

---
+++ Yellow_Belt/script.js
@@ -0,0 +1,10 @@
+
+function alertMessage(element){
+    alert("Your cart is empty")
+}
+
+
+function closeOne(element) {
+    var div = document.getElementById("acceptAllCookies").innerHTML = acceptAllCookies;
+    div.style.display = 'none'
+}
